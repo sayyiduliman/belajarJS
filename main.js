98823d4a44e3de0923fa82377c786852f3ae4b9c
sayyiduliman <saynashcloth@gmail.com>
2023-10-12 16:49:42 +0700

    update index.html

---
+++ main.js
@@ -0,0 +1,48 @@
+// console.log("hahahahaha")
+// alert("test notif")
+// prompt("pinjam 1 juta")
+
+// var Promnet = "coding sangat mudah"
+// console.log(Promnet)
+
+// var Promnet = "coding sulit tapi bisa"
+// console.log(Promnet)
+
+//var Promnet = "semua pasti bisa"
+//console.log(Promnet)
+
+// let Promnet = "coding menyenangkan"
+// console.log(Promnet)
+// let Promnet = "coding menyenangkan"
+// console.log(Promnet)
+
+// const Promnet = "coding bagian hidupku"
+// console.log(Promnet)
+// const Promnet "coding bukan bagian hidupku"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Masukkan Poin Anda")
+//    if(totalPoin >= 100){
+//        document.write("Congratssss")
+//    } else{
+//        document.write("teruslah nyoba")
+//    }
+
+    let x = 6;
+    let y = 3;
+
+    console.log (x<10 && y > 1) 
+    console.log (x<10 && y < 1)
+    console.log (x==5||y==5)
+    console.log (x==6||y==5) 
+    console.log (!(x==y))
+
+    document.write (x<10 && y > 1)
+    document.write ("<br>") 
+    document.write (x<10 && y < 1)
+    document.write ("<br>")
+    document.write (x==5||y==5)
+    document.write ("<br>")
+    document.write (x==6||y==5)
+    document.write ("<br>")
+    document.write (!(x==y))
